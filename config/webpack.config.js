a3e9ee5ffab4ce371b191706c0f88f9be11b9a11
Nhan Nguyen <nhan1303@gmail.com>
2022-10-20 18:31:19 +0700

    Config Module Alias

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -311,6 +311,8 @@ module.exports = function (webpackEnv) {
         .map((ext) => `.${ext}`)
         .filter((ext) => useTypeScript || !ext.includes("ts")),
       alias: {
+        "@components": path.resolve(__dirname, "../src/components/"),
+        "@utils": path.resolve(__dirname, "../src/utils/"),
         // Support React Native Web
         // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
         "react-native": "react-native-web",
